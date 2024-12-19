797520b6f614e1b7b6e0202a42a390d608e8bea8
ReStranger <strengerplayr@gmail.com>
2024-12-20 00:27:25 +0300

    src: Init QuickSettings

---
+++ src/widgets/quicksettings/QuickSettings.tsx
@@ -0,0 +1,35 @@
+import { Astal, App, Gtk } from "astal/gtk3";
+import PopupWindow from "../PopupWindow";
+import { toggleWindow } from "../../lib/utils";
+
+export default function () {
+    return (
+        <PopupWindow
+            valign={Gtk.Align.START}
+            visible={false}
+            //margin={12}
+            name="quick-settings"
+            namespace="quick-settings"
+            className="QuickSettings"
+            layer={Astal.Layer.OVERLAY}
+            exclusivity={Astal.Exclusivity.NORMAL}
+            keymode={Astal.Keymode.EXCLUSIVE}
+            anchor={Astal.WindowAnchor.TOP | Astal.WindowAnchor.RIGHT}
+            onKeyPressEvent={(self, event) => {
+                const [keyEvent, keyCode] = event.get_keycode();
+                if (keyEvent && keyCode == 9) {
+                    toggleWindow(self.name);
+                }
+            }}
+        >
+            {" "}
+            <box className="QuickSettings" vertical valign={Gtk.Align.START}>
+                <button>sss</button>
+                <button>sss</button>
+                <button>sss</button>
+                <button>sss</button>
+                <button>sss</button>
+            </box>
+        </PopupWindow>
+    );
+}
