e673c6f40c02f6168bfc9ec589555615fa918961
ReStranger <strengerplayr@gmail.com>
2024-12-19 03:29:32 +0300

    Apply formatting
    Signed-off-by: ReStranger <strengerplayr@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,11 @@ import scss from "./styles/index.scss";
 import Bar from "./widgets/bar/Bar";
 
 export default function () {
-  App.start({
-    css: scss,
-    gtkTheme: "adw-gtk3",
-    main() {
-      App.get_monitors().map(Bar);
-    },
-  });
+    App.start({
+        css: scss,
+        gtkTheme: "adw-gtk3",
+        main() {
+            App.get_monitors().map(Bar);
+        },
+    });
 }
