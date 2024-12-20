8ffddb8071c10706f01618311bc8881f70bd6490
ReStranger <strengerplayr@gmail.com>
2024-12-20 03:54:40 +0300

    widgets/bar: Add clock button

---
+++ src/widgets/bar/buttons/Clock.tsx
@@ -0,0 +1,56 @@
+import { GLib, Variable } from "astal";
+import BarButton from "./BarButton";
+import { toggleWindow } from "../../../lib/utils";
+
+interface DateTimeFormatProps {
+    format: string;
+}
+
+const DateTimeFormat = ({ format }: DateTimeFormatProps) => {
+    const time = Variable<string>("").poll(1000, () => {
+        return GLib.DateTime.new_now_local().format(format)!;
+    });
+    return (
+        <label
+            className="format"
+            onDestroy={() => time.drop()}
+            label={time()}
+        />
+    );
+};
+const Time = () => {
+    return (
+        <BarButton
+            className="Time"
+            cursor="pointer"
+            onClicked={() => {
+                toggleWindow("quick-settings");
+            }}
+        >
+            <DateTimeFormat format="%H:%M" />
+        </BarButton>
+    );
+};
+
+const Date = () => {
+    return (
+        <BarButton
+            className="Date"
+            cursor="pointer"
+            onClicked={() => {
+                toggleWindow("quick-settings");
+            }}
+        >
+            <DateTimeFormat format="%a %e" />
+        </BarButton>
+    );
+};
+
+export default () => {
+    return (
+        <box className="Clock">
+            <Time />
+            <Date />
+        </box>
+    );
+};
