a19416b61c45d188a96b4f9ab4577d34858d81ae
ReStranger <strengerplayr@gmail.com>
2024-12-20 04:06:45 +0300

    lib: Add handle utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { App } from "astal/gtk3";
+import Apps from "gi://AstalApps";
 import { popupWindowNames } from "./variables";
 
 export function activePopupWindows() {
@@ -15,4 +16,41 @@ export function toggleWindow(windowName: string) {
         return;
     }
     window.visible = !window.visible;
+}
+
+export function isMathExpression(input: string): boolean {
+    return /^[\d+\-*/().\s^]+$/.test(input);
+}
+
+export function evaluateExpression(input: string): number | null {
+    try {
+        const preparedInput = input.replace(/\^/g, "**");
+        const result = Function(`"use strict"; return (${preparedInput})`)();
+        if (typeof result === "number" && isFinite(result)) {
+            return result;
+        }
+        return null;
+    } catch (error) {
+        return null;
+    }
+}
+
+export function handleInput(
+    input: string,
+    apps: Apps.Apps,
+): string | null | Apps.Application {
+    input = input.trim();
+    if (isMathExpression(input)) {
+        const evaluated = evaluateExpression(input);
+        if (evaluated !== null) {
+            return String(evaluated);
+        }
+    }
+
+    const app = apps.fuzzy_query(input)?.[0];
+    if (app) {
+        return app;
+    }
+
+    return null;
 }
