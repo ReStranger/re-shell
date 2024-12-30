38bee94363594b2045a6c3e92ca0b72c317dc359
ReStranger <strengerplayr@gmail.com>
2024-12-30 04:53:49 +0300

    src/widgets/quicksettings/sliders: Add export to sliders
    Signed-off-by: ReStranger <strengerplayr@gmail.com>

--- src/widgets/quicksettings/sliders/AudioSlider.tsx
+++ src/widgets/quicksettings/sliders/AudioSlider.tsx
@@ -2,16 +2,19 @@ import { bind } from "astal";
 import Wp from "gi://AstalWp";
 
 export default function () {
-    const speaker = Wp.get_default()?.audio.defaultSpeaker!;
-
-    return (
-        <box className="AudioSlider">
-            <icon icon={bind(speaker, "volumeIcon")} />
-            <slider
-                hexpand
-                onDragged={({ value }) => (speaker.volume = value)}
-                value={bind(speaker, "volume")}
-            />
-        </box>
-    );
+    try {
+        const speaker = Wp.get_default()?.audio.defaultSpeaker!;
+        return (
+            <box className="AudioSlider">
+                <icon icon={bind(speaker, "volumeIcon")} />
+                <slider
+                    hexpand
+                    onDragged={({ value }) => (speaker.volume = value)}
+                    value={bind(speaker, "volume")}
+                />
+            </box>
+        );
+    } catch (error) {
+        return <box className="AudioSlider" />;
+    }
 }
--- src/widgets/quicksettings/sliders/BrightnessSlider.tsx
+++ src/widgets/quicksettings/sliders/BrightnessSlider.tsx
@@ -4,14 +4,20 @@ import icons from "../../../lib/icons";
 import BrightnessService from "../../../lib/services/brightness";
 
 export default function () {
-    return (
-        <box className="BrightnessSlider">
-            <icon icon={icons.brightness.screen} />
-            <slider
-                hexpand
-                onDragged={({ value }) => (BrightnessService!.screen = value)}
-                value={bind(BrightnessService).as((b) => b.screen)}
-            />
-        </box>
-    );
+    try {
+        return (
+            <box className="BrightnessSlider">
+                <icon icon={icons.brightness.screen} />
+                <slider
+                    hexpand
+                    onDragged={({ value }) =>
+                        (BrightnessService!.screen = value)
+                    }
+                    value={bind(BrightnessService).as((b) => b.screen)}
+                />
+            </box>
+        );
+    } catch (error) {
+        return <box className="BrightnessSlider" />;
+    }
 }
