4eda6f5f84061af1e7201894d5af26684efafdc5
ReStranger <restranger@disroot.org>
2025-03-25 05:06:33 +0300

    feat: Add configs for linter and formatter
    Signed-off-by: ReStranger <restranger@disroot.org>

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+    { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+    {
+        files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+        languageOptions: { globals: globals.node },
+    },
+    {
+        files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+        plugins: { js },
+        extends: ["js/recommended"],
+
+        rules: {
+            "no-unused-vars": "warn",
+            indent: ["error", 4, { SwitchCase: 1 }],
+            quotes: ["error", "double"],
+        },
+    },
+    tseslint.configs.recommended,
+]);
