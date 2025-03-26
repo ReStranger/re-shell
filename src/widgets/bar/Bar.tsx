8fc3361c38e83c614cb81ae2f0b4d946f7ca11c0
ReStranger <restranger@disroot.org>
2025-03-27 00:03:16 +0300

    feet(bar): Initial realisation of Bar
    Signed-off-by: ReStranger <restranger@disroot.org>

--- src/widgets/bar/Bar.tsx
+++ src/widgets/bar/Bar.tsx
@@ -1,36 +1,34 @@
-import { App, Astal, Gtk, Gdk } from "astal/gtk4"
-import { Variable } from "astal"
+import { App, Astal, Gdk } from "astal/gtk4";
 
-const time = Variable("").poll(1000, "date")
+const Left = () => <box></box>;
+
+const Center = () => (
+    <box>
+        <label label="ags" />
+    </box>
+);
+
+const Right = () => <box></box>;
 
 export default function Bar(gdkmonitor: Gdk.Monitor) {
-    const { TOP, LEFT, RIGHT } = Astal.WindowAnchor
+    const { TOP, LEFT, RIGHT } = Astal.WindowAnchor;
 
-    return <window
-        visible
-        cssClasses={["Bar"]}
-        gdkmonitor={gdkmonitor}
-        exclusivity={Astal.Exclusivity.EXCLUSIVE}
-        anchor={TOP | LEFT | RIGHT}
-        application={App}>
-        <centerbox cssName="centerbox">
-            <button
-                onClicked="echo hello"
-                hexpand
-                halign={Gtk.Align.CENTER}
-            >
-                Welcome to AGS!
-            </button>
-            <box />
-            <menubutton
-                hexpand
-                halign={Gtk.Align.CENTER}
-            >
-                <label label={time()} />
-                <popover>
-                    <Gtk.Calendar />
-                </popover>
-            </menubutton>
-        </centerbox>
-    </window>
+    return (
+        <window
+            visible
+            cssClasses={["bar"]}
+            name={"bar"}
+            namespace={"bar"}
+            gdkmonitor={gdkmonitor}
+            exclusivity={Astal.Exclusivity.EXCLUSIVE}
+            anchor={TOP | LEFT | RIGHT}
+            application={App}
+        >
+            <centerbox cssName="centerbox">
+                <Left />
+                <Center />
+                <Right />
+            </centerbox>
+        </window>
+    );
 }
