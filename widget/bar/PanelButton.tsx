69b7817f167b957af91a8efc8649f2167ea4f2c7
ReStranger <restranger@disroot.org>
2025-06-19 16:51:34 +0300

    feat(bar): Add PanelButton
    Signed-off-by: ReStranger <restranger@disroot.org>

---
+++ widget/bar/PanelButton.tsx
@@ -0,0 +1,52 @@
+import { Gtk } from "ags/gtk4";
+import { toggleClassName } from "lib/utils";
+import { CCProps } from "ags";
+import options from "options";
+import app from "ags/gtk4/app";
+
+type PanelButtonProps = CCProps<Gtk.Button, Gtk.Button.ConstructorProps> & {
+    window?: string;
+    flat?: boolean;
+};
+
+export default ({
+    window = "",
+    flat,
+    children,
+    $,
+    ...rest
+}: PanelButtonProps) => (
+    <button
+        $={(self) => {
+            let open = false;
+            toggleClassName(self, "panel-button");
+            toggleClassName(self, window);
+
+            toggleClassName(
+                self,
+                "flat",
+                flat ?? options.bar.flatButtons.getValue(),
+            );
+            app.connect(
+                "window-toggled",
+                (_, win: string, visible: boolean) => {
+                    if (win !== window) return;
+
+                    if (open && !visible) {
+                        open = false;
+                        toggleClassName(self, "active", false);
+                    }
+
+                    if (visible) {
+                        open = true;
+                        toggleClassName(self, "active");
+                    }
+                },
+            );
+            if ($) $(self);
+        }}
+        {...rest}
+    >
+        <box>{children}</box>
+    </button>
+);
